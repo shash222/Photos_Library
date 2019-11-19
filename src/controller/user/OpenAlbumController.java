da585067731df9ffb739b8ff6cfa70b9795d6563
Mohammed Alnadi <mohammed.alnadi@gmail.com>
2019-11-19 16:25:19 -0500

    modify Caption

--- src/controller/user/OpenAlbumController.java
+++ src/controller/user/OpenAlbumController.java
@@ -1,6 +1,7 @@
 package controller.user;
 
 import controller.Photos;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
@@ -23,68 +24,69 @@ import java.util.ResourceBundle;
 
 // TODO Fix bug where list doesn't update unless this view is reopened
 public class OpenAlbumController implements Initializable {
-    @FXML
-    TableView photoTable;
-    @FXML
-    private TableColumn imageColumn;
-    @FXML
-    private TableColumn captionColumn;
-    @FXML
-    private TableColumn dateTakenColumn;
+	@FXML
+	TableView photoTable;
+	@FXML
+	private TableColumn imageColumn;
+	@FXML
+	private TableColumn captionColumn;
+	@FXML
+	private TableColumn dateTakenColumn;
 
-    static AlbumEntry selectedEntry;
-    static Photo selectedPhoto;
-    static TableView staticPhotoTable;
+	static AlbumEntry selectedEntry;
+	static Photo selectedPhoto;
+	static TableView staticPhotoTable;
 
-    List<Photo> photos = new ArrayList<>();
-    public static final String ALBUM_PATH_FORMAT = "src/resources/USERS/%s/%s.txt";
+	List<Photo> photos = new ArrayList<>();
+	public static final String ALBUM_PATH_FORMAT = "src/resources/USERS/%s/%s.txt";
 
-    private void setPhotos() {
-        String albumPath = String.format(ALBUM_PATH_FORMAT, Photos.currentUser, UserController.selectedAlbum);
-        List<Photo> photosInAlbum = Utilities.readSerializedObjectFromFile(albumPath);
-        imageColumn.setCellValueFactory(new PropertyValueFactory<>("image"));
-        captionColumn.setCellValueFactory(new PropertyValueFactory<>("caption"));
-        dateTakenColumn.setCellValueFactory(new PropertyValueFactory<>("dateTaken"));
-        for (Photo p : photosInAlbum) {
-            AlbumEntry albumEntry = new AlbumEntry(p.getLocation(), p.getCaption(), p.getDateTaken(), p);
-            photoTable.getItems().add(albumEntry);
-        }
-        staticPhotoTable = photoTable;
-    }
+	private void setPhotos() {
+		String albumPath = String.format(ALBUM_PATH_FORMAT, Photos.currentUser, UserController.selectedAlbum);
+		List<Photo> photosInAlbum = Utilities.readSerializedObjectFromFile(albumPath);
+		imageColumn.setCellValueFactory(new PropertyValueFactory<>("image"));
+		captionColumn.setCellValueFactory(new PropertyValueFactory<>("caption"));
+		dateTakenColumn.setCellValueFactory(new PropertyValueFactory<>("dateTaken"));
+		for (Photo p : photosInAlbum) {
+			AlbumEntry albumEntry = new AlbumEntry(p.getLocation(), p.getCaption(), p.getDateTaken(), p);
+			photoTable.getItems().add(albumEntry);
+		}
+		staticPhotoTable = photoTable;
+	}
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        setPhotos();
-    }
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		setPhotos();
+	}
 
-    @FXML
-    public void updateSelected(MouseEvent mouseEvent) {
-        AlbumEntry newSelectedEntry = (AlbumEntry) photoTable.getSelectionModel().getSelectedItem();
-        if (newSelectedEntry != null) {
-            selectedEntry = (AlbumEntry) photoTable.getSelectionModel().getSelectedItem();
-            selectedPhoto = selectedEntry.getAssociatedPhoto();
-        }
-    }
+	@FXML
+	public void updateSelected(MouseEvent mouseEvent) {
+		AlbumEntry newSelectedEntry = (AlbumEntry) photoTable.getSelectionModel().getSelectedItem();
+		if (newSelectedEntry != null) {
+			selectedEntry = (AlbumEntry) photoTable.getSelectionModel().getSelectedItem();
+			selectedPhoto = selectedEntry.getAssociatedPhoto();
+		}
+	}
 
-    @FXML
-    public void addPhoto(MouseEvent mouseEvent) {
-        Utilities.displayView("user/AddPhotoView.fxml");
-        System.out.println("Before");
-        setPhotos();
-        photoTable.refresh();
-        System.out.println("After");
-    }
+	@FXML
+	public void addPhoto(MouseEvent mouseEvent) {
+		Utilities.displayView("user/AddPhotoView.fxml");
+		System.out.println("Before");
+		setPhotos();
+		photoTable.refresh();
+		System.out.println("After");
+	}
 
-    @FXML
-    public void removePhoto(MouseEvent mouseEvent) {
-        if (selectedEntry != null) Utilities.displayAlert(Alert.AlertType.ERROR, "No entry selected");
-        else 
-    }
+	@FXML
+	public void removePhoto(MouseEvent mouseEvent) {
+		if (selectedEntry != null)
+			Utilities.displayAlert(Alert.AlertType.ERROR, "No entry selected");
+		// else
+	}
 
-    // TODO doesn't work, finish
-    @FXML
-    public void modifyCaption(MouseEvent mouseEvent) {
-    	if (selectedEntry == null)
+	// TODO doesn't work, finish
+	@FXML
+	public void modifyCaption(MouseEvent mouseEvent) {
+		if (selectedEntry == null)
 			Utilities.displayAlert(Alert.AlertType.ERROR, "No entry selected");
 		else {
 			TextInputDialog dialog = new TextInputDialog();
@@ -95,39 +97,55 @@ public class OpenAlbumController implements Initializable {
 			// Traditional way to get the response value.
 			Optional<String> result = dialog.showAndWait();
 			if (result.isPresent()) {
+				String ALBUM_PATH_FORMAT = "src/resources/USERS/%s/%s.txt";
+
+				String albumPath = String.format(ALBUM_PATH_FORMAT, Photos.currentUser, UserController.selectedAlbum);
+
 				selectedEntry.setCaption(result.get());
+				List<Photo> photosInAlbum = new ArrayList();
+
+				ObservableList items = photoTable.getItems();
+				for (Object item : items) {
+					photosInAlbum.add(((AlbumEntry) item).getAssociatedPhoto());
+				}
+				Utilities.writeSerializedObjectToFile(photosInAlbum, albumPath);
+				setPhotos();
+				photoTable.refresh();
+
 			}
 		}
-    }
+	}
 
-    @FXML
-    public void displayPhoto(MouseEvent mouseEvent) {
-        if (selectedEntry == null) Utilities.displayAlert(Alert.AlertType.ERROR, "No entry selected");
-        else Utilities.displayView("user/DisplayPhotoView.fxml");
-    }
+	@FXML
+	public void displayPhoto(MouseEvent mouseEvent) {
+		if (selectedEntry == null)
+			Utilities.displayAlert(Alert.AlertType.ERROR, "No entry selected");
+		else
+			Utilities.displayView("user/DisplayPhotoView.fxml");
+	}
 
-    @FXML
-    public void addTag(MouseEvent mouseEvent) {
+	@FXML
+	public void addTag(MouseEvent mouseEvent) {
 
-    }
+	}
 
-    @FXML
-    public void removeTag(MouseEvent mouseEvent) {
+	@FXML
+	public void removeTag(MouseEvent mouseEvent) {
 
-    }
+	}
 
-    @FXML
-    public void copyToNewAlbum(MouseEvent mouseEvent) {
+	@FXML
+	public void copyToNewAlbum(MouseEvent mouseEvent) {
 
-    }
+	}
 
-    @FXML
-    public void moveToNewAlbum(MouseEvent mouseEvent) {
+	@FXML
+	public void moveToNewAlbum(MouseEvent mouseEvent) {
 
-    }
+	}
 
-    @FXML
-    public void viewSlideshow(MouseEvent mouseEvent) {
-        Utilities.displayView("user/ViewSlideshowView.fxml");
-    }
+	@FXML
+	public void viewSlideshow(MouseEvent mouseEvent) {
+		Utilities.displayView("user/ViewSlideshowView.fxml");
+	}
 }
